402eb3916ddebb3941fe505feab944c2e2fc7a28
Dan Grabowski <dmgrabowski@gmail.com>
2018-12-13 09:22:49 -0500

    some decoder impl

--- Decoder.cpp
+++ Decoder.cpp
@@ -1,5 +1,7 @@
 #include "Decoder.hpp"
 
+#include <iostream>
+
 namespace Fm68000 {
 
 Decoder::Decoder()
@@ -17,6 +19,34 @@ Uop::Pointer Decoder::Decode(const uint16_t &uop)
     switch ( (uop & 0b1111000000000000) >> 12 )
     {
         case 0b0000:
+            if (uop & 0b0000000100000000)
+            {
+                if (uop & 0b0000000000001000) 
+                {
+                    std::cout << "MOVEP" << std::endl;
+                }
+                else 
+                {
+                    switch ((uop & 0b0000000011000000) >> 6) {
+                        case 0b00 :
+                            std::cout << "BTST" << std::endl;
+                        break;
+                        case 0b01 :
+                            std::cout << "BCHG" << std::endl;
+                        break;
+                        case 0b10 : 
+                            std::cout << "BSET" << std::endl;
+                        break;
+                        case 0b11 : 
+                            std::cout << "BCLR" << std::endl;
+                        break;
+                    };
+                }
+            }
+            else
+            {
+
+            }
             break;
         case 0b0001: 
         case 0b0010:
